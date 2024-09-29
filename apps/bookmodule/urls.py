014a9201f5b0e891077d743ff52b7fd9dde7e3e0
Danah-18 <431201338@qu.edu.sa>
2024-09-29 04:00:03 +0300

    Lab 3

--- apps/bookmodule/urls.py
+++ apps/bookmodule/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.index),  # Maps the root URL of the bookmodule to the index view
-    path('index2/<int:val1>/<int:val2>/', views.index2),  # Maps a URL with two integer parameters to the index2 view
-    path('<int:bookId>/', views.viewbook),  # Adding a new path for book details
+urlpatterns = [ 
+    path('', views.index, name="books.index"), 
+    path('list_books/', views.list_books, name="books.list_books"), 
+    path('<int:bookId>/', views.viewbook, name="books.view_one_book"), 
+    path('aboutus/', views.aboutus, name="books.aboutus"), 
 ]
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -1,28 +1,31 @@
-from django.shortcuts import render
-from django.shortcuts import render 
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+# Sample data for demonstration purposes
+BOOKS = [
+    {'id': 123, 'title': 'Continuous Delivery', 'author': 'J. Humble and D. Farley'},
+    {'id': 456, 'title': 'Secrets of Reverse Engineering', 'author': 'E. Eilam'},
+    {'id': 1, 'title': 'Internet & World Wide Web How To Program', 'author': 'Deitel, Deitel'}
+
+]
 
 def index(request):
-    name = request.GET.get("name") or "world!"
-    return render(request, "bookmodule/index.html" , {"name": name})    
+    name = request.GET.get("name", "world!")
+    return render(request, "bookmodule/index.html", {"name": name})
 
-
-def index2(request, val1=0, val2=0):
-    return HttpResponse(f"value1 = {val1} and value2 = {val2}")
-
+def list_books(request):
+    # Pass sample data to the list_books template
+    return render(request, 'bookmodule/list_books.html', {'books': BOOKS})
 
 def viewbook(request, bookId):
-    # Mock data: assume these are books in your database
-    book1 = {'id': 123, 'title': 'Continuous Delivery', 'author': 'J. Humble and D. Farley'}
-    book2 = {'id': 456, 'title': 'Secrets of Reverse Engineering', 'author': 'E. Eilam'}
-    targetBook = None
+    # Find the book by ID or return a 404 error if not found
+    targetBook = next((book for book in BOOKS if book['id'] == bookId), None)
+    
+    # If the book is not found, use get_object_or_404 for a proper 404 response
+    if targetBook:
+        context = {'book': targetBook}
+        return render(request, 'bookmodule/show.html', context)  # Use the show.html for details
+    else:
+        return render(request, 'bookmodule/one_book.html', {'book': None})  # Handle not found scenario
 
-    if book1['id'] == bookId:
-        targetBook = book1
-    if book2['id'] == bookId:
-        targetBook = book2
-
-    context = {'book': targetBook}  # Passing the book data to the template
-    return render(request, 'bookmodule/show.html', context)
-
-# Create your views here.
+def aboutus(request):
+    return render(request, 'bookmodule/aboutus.html')
